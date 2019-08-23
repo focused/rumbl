1a79acfc3d70f499c43bce2183160e9b754dd9a6
Sergei Maximov <s.b.maximov@gmail.com>
2019-08-23 15:22:03 +0300

    Fix authentication for proxied websocket connection

--- assets/js/socket.js
+++ assets/js/socket.js
@@ -1,6 +1,6 @@
 import {Socket} from "phoenix"
 
-let socket = new Socket("/socket", {
+let socket = new Socket("ws://localhost:4100/socket", {
   params: {token: window.userToken},
   logger: (kind, msg, data) => { console.log(`${kind}: ${msg}`, data) }
 })
